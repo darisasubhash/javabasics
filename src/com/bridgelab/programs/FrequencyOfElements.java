b279c023d6d1e21649f2e441b093ab28b3068ed5
darisasubhash <subbusubbu8193@gmail.com>
2026-01-28 11:00:28 +0530

    duplicate check and frequency check

---
+++ src/com/bridgelab/programs/FrequencyOfElements.java
@@ -0,0 +1,25 @@
+package com.bridgelab.programs;
+
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
+
+public class FrequencyOfElements {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        int n=scan.nextInt();
+        int array[]=new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i]=scan.nextInt();
+        }
+        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<Integer, Integer>();
+        for(Integer a:array) {
+            map.put(a,map.getOrDefault(a, 0)+1);
+        }
+        Set<Entry<Integer, Integer>> entries = map.entrySet();
+        for(Entry<Integer, Integer> e:entries) {
+            System.out.println(e.getKey()+" "+e.getValue());
+        }
+    }
+}
