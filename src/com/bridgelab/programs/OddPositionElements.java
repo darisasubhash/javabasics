d0a18e4b9cfdd910fa5fdae5abffb8168420c6f3
darisasubhash <subbusubbu8193@gmail.com>
2026-01-28 10:50:11 +0530

    even index and odd index elements

---
+++ src/com/bridgelab/programs/OddPositionElements.java
@@ -0,0 +1,18 @@
+package com.bridgelab.programs;
+
+import java.util.Scanner;
+
+public class OddPositionElements {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n= scan.nextInt();
+        int array[]=new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i]=scan.nextInt();
+        }
+        for (int i = 1; i < array.length; i=i+2) {
+            System.out.println(array[i]);
+        }
+
+    }
+}
